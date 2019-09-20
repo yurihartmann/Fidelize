99e47b74d7f02baf1b0101aaabce484866243dc4
Cainan <cainan.web@gmail.com>
2019-09-20 11:38:50 -0300

    DEU SERTO

--- fidelize/ajax/querofidelizar.php
+++ fidelize/ajax/querofidelizar.php
@@ -4,7 +4,7 @@ require_once "site.php";
 $nome = $_POST['nome'];	
 $email = $_POST['email'];
 $descricao = $_POST['mensagem'];
-$tipo = $_POST['tipo'];
+$tipo = $_POST['plano'];
 
 $sqlSuporte = "INSERT INTO fidelizar VALUES (DEFAULT, '$nome', '$email', '$descricao' , '$tipo');";
 $querySuporte = mysqli_query($conexao, $sqlSuporte);
