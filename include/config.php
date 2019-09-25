9d6618247d54beb6b520f8549a61f63ac3f62cb5
Yuri Hartmann <yurihartmann0607@gmail.com>
2019-09-25 11:51:06 -0300

    arquivo config

---
+++ include/config.php
@@ -0,0 +1,17 @@
+<?php
+
+	/** 
+	 * CONFIGURAÇÕES
+     *
+     * ATIVE E DESATIVE O SMS, CASO ATIVADO COLOQUE UMA KEY E UMA SECRET DA AWS COM ERMISSAO PARA SNS_SMS
+     *
+	 */
+
+//error_reporting(0);
+date_default_timezone_set('America/Sao_Paulo');
+
+CONST SMS_ACTIVE = false;
+CONST SNS_KEY = "";
+CONST SNS_SECRET = "";
+
+?>
