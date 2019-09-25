5e0ab2b2cf37f0a67d763e239de39221327123a6
Yuri Hartmann <yurihartmann0607@gmail.com>
2019-09-25 11:33:30 -0300

    contante de sms

--- cliente/include/sms.php
+++ cliente/include/sms.php
@@ -4,33 +4,36 @@ use Aws\Sns\SnsClient;
 use Aws\Exception\AwsException;
 
 
-function sendSMS($numero, $mensagem){
+function sendSMS($numero, $mensagem)
+{
 
+    if (SMS_ACTIVE) {
 
-    $SnSclient = new SnsClient([
-        'credentials' => ['key' => 'AKIA2ZZM2LL4FW3JSEQ2', 'secret' => '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'],
-        'region' => 'us-west-2',
-        'version' => '2010-03-31'
-    ]);
-
-    $phone = $numero;
-    $phone = "+55" .$phone;
-    $message = $mensagem;
-    $message = limitaTexto(130, $message);
-
-    try {
-        $result = $SnSclient->SetSMSAttributes([
-            'attributes' => [
-                'DefaultSMSType' => 'Transactional',
-            ],
+        $SnSclient = new SnsClient([
+            'credentials' => ['key' => SNS_KEY, 'secret' => SNS_SECRET],
+            'region' => 'us-west-2',
+            'version' => '2010-03-31'
         ]);
-        $result = $SnSclient->publish([
-            'Message' => $message,
-            'PhoneNumber' => $phone,
-        ]);
-    } catch (AwsException $e) {
-        // output error message if fails
-        $e->getMessage();
+
+        $phone = $numero;
+        $phone = "+55" . $phone;
+        $message = $mensagem;
+        $message = limitaTexto(130, $message);
+
+        try {
+            $result = $SnSclient->SetSMSAttributes([
+                'attributes' => [
+                    'DefaultSMSType' => 'Transactional',
+                ],
+            ]);
+            $result = $SnSclient->publish([
+                'Message' => $message,
+                'PhoneNumber' => $phone,
+            ]);
+        } catch (AwsException $e) {
+            // output error message if fails
+            $e->getMessage();
+        }
     }
 
     sleep(1);
--- include/config.php
+++ include/config.php
@@ -8,4 +8,8 @@
 //error_reporting(0);
 date_default_timezone_set('America/Sao_Paulo');
 
+CONST SMS_ACTIVE = false;
+CONST SNS_KEY = "";
+CONST SNS_SECRET = "";
+
 ?>
