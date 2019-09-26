ce8b0f9c505657ed62e054fc6aeb75d0888fec62
Yuri Hartmann <yurihartmann0607@gmail.com>
2019-09-26 10:09:45 -0300

    include config consertado

--- fidelize/ajax/site.php
+++ fidelize/ajax/site.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "../../include/config.php";
+require_once "../include/config.php";
 
 $conexao = mysqli_connect(LOCAL, USER, PASS, DB) or die ("Erro na conexao com o servidor.");
 
