62d72ffda8b4bff4184e1f8d36bad5e43ca522bf
William Basso <williambasso73@gmail.com>
2019-10-01 11:51:40 -0300

    Envio token feito

---
+++ include/template_token/token.php
@@ -0,0 +1,23 @@
+<?php include_once "header.php" ?>
+<div class="banner">
+	<!-- <img src="../../media/images/email/empresa.jpg" alt="fundo"> -->
+</div>
+<div class="one-col">
+	<h1>Parabéns, você recebeu um Token</h1>
+	<p>Olá "Nome", você completou seu cupom e agora está recebendo o token para retirar seu prêmio na loja!</p>
+	<div style="text-align: center; margin: 20px 0px">
+		<img height="200px" src="https://fidelize.ga/uploads/379910744.png">
+		<h3 style="padding-top: 10px;">"Nome do cartão"</h3>
+	</div>
+	<h1 style="padding-top: 20px;">Número do seu token:  <span class="token">"99999"</span></h1>
+	<h1 style="">Loja:<span class="token">"LOJA"</span></h1>
+	<h1 style="">Prêmio:<span class="token">"PRÊMIO"</span></h1>
+	<div class="button-holder">
+		<a class="btn" href="https://fidelize.ga/cliente/meus_tokens.php" target="_blank"><ELEMENT>Ver Token</ELEMENT></a>
+	</div>
+	<div class="line"></div>
+</div>
+
+
+
+<?php include_once "footer.php" ?>
